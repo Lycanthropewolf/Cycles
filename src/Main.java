0f11659e49f75a4c2f91c511766fb9bcf21595e3
Антон <antonolegovich2017@icloud.com>
2022-08-01 16:33:06 +0600

    Домашка циклы

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задание 1
+        int a = 0;
+        while (a < 10) {
+            a = a + 1; System.out.println(" итерация числа а равна " + a);
+        }
+        for (a = 10; a > 0; a = a -1){
+            System.out.println(" итерация числа а равна " + a);
+        }
+
+        // Задание 2
+
 
     }
 
